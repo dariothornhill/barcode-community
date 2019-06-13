045611c0eed7ca607d7f9e744e14e5a1ac82b840
Dario Thornhill <dario.thornhill@gmail.com>
2019-06-13 02:47:38 -0400

    Added feature to update and delete profiles. Resolves #1

--- src/containers/Members.js
+++ src/containers/Members.js
@@ -5,11 +5,11 @@ import ProfileCard from '../components/ProfileCard';
 import { AuthContext } from '../context/AuthContext';
 
 const Members = props => {
-  const [currentUser, setCurrentUser, handleLogin, handleLogout] = useContext(
-    AuthContext
-  );
+  const [currentUser, setCurrentUser, handleLogin, handleLogout] = useContext(AuthContext);
 
   const [members, setMembers] = useState([]);
+  const [hasProfile, setHasProfile] = useState(false);
+
   useEffect(() => {
     db.collection('members')
       // .doc(threadId)
@@ -24,45 +24,64 @@ const Members = props => {
 
         setMembers(threads);
       });
-  }, []);
-  console.log(currentUser);
+    const email = currentUser && 'email' in currentUser ? currentUser.email : false;
+    db.collection('members')
+      .doc(email)
+      .get()
+      .then(x => setHasProfile(x.exists));
+  }, [currentUser, setMembers, setHasProfile]);
+
   const handleAddMember = newMember => {
     newMember.creator_id = currentUser.user_id;
     console.log(newMember);
-    db.collection('members').add(newMember);
+    db.collection('members')
+      .doc(currentUser.email)
+      .set(newMember)
+      .then(x => {
+        setHasProfile(true);
+      });
   };
+  const handleDeleteMember = () => {
+    db.collection('members')
+      .doc(currentUser.email)
+      .delete()
+      .then(() => {
+        setHasProfile(false);
+      });
+  };
+
   return (
-    <div className='container'>
-      <div class='docs-content'>
-        <div class='columns' style={{ padding: '30px' }}>
-          <div class='column col-8 col-sm-12'>
+    <div className="container">
+      <div class="docs-content">
+        <div class="columns" style={{ padding: '30px' }}>
+          <div class="column col-8 col-sm-12">
             {members ? (
-              members.map(item => <ProfileCard {...item} />)
+              members.map(item => <ProfileCard {...item} key={item.creator_id} />)
             ) : (
-              <div class='empty' style={{ height: '100vh' }}>
-                <div class='empty-icon'>
-                  <i class='icon icon-3x icon-people' />
+              <div class="empty" style={{ height: '100vh' }}>
+                <div class="empty-icon">
+                  <i class="icon icon-3x icon-people" />
                 </div>
-                <p class='empty-title h5'>There appears to be no one here</p>
-                <p class='empty-subtitle'>
-                  Leave the computer and go find some friends
-                </p>
+                <p class="empty-title h5">There appears to be no one here</p>
+                <p class="empty-subtitle">Leave the computer and go find some friends</p>
               </div>
             )}
           </div>
-          <div class='column col-4 col-sm-12'>
-            <div class='card box'>
-              <div class='card-body'>
-                <h3>Create Profile</h3>
+          <div class="column col-4 col-sm-12">
+            <div class="card box">
+              <div class="card-body">
+                <h3>{hasProfile === false ? 'Create' : 'Update'} Profile</h3>
                 <span>
-                  By creating a developer profile, your skills and experience
-                  will be visible to other members, potential employers and
-                  investors alike
+                  By creating a developer profile, your skills and experience will be visible to other members,
+                  potential employers and investors alike
                 </span>
-                {currentUser ? (
-                  <MembershipForm onSubmit={handleAddMember} />
+                {currentUser ? <MembershipForm onSubmit={handleAddMember} /> : <p>Sorry but you need to login first</p>}
+                {currentUser && hasProfile ? (
+                  <button type="submit" class="btn btn-secondary" onClick={handleDeleteMember}>
+                    Delete Profile {console.log(currentUser && hasProfile)}
+                  </button>
                 ) : (
-                  <p>Sorry but you need to login first</p>
+                  ''
                 )}
               </div>
             </div>
